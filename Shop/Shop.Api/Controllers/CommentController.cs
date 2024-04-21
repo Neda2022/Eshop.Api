a41aa62816da7b95a9c134a42f17bf362ff671e0
neda.honarmand82@gmail.com <neda.honarmand82@gmail.com>
2024-04-21 18:35:08 +0330

    87_project-comment_controller

---
+++ Shop/Shop.Api/Controllers/CommentController.cs
@@ -0,0 +1,65 @@
+﻿using Common.Asp.NetCore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Application.Comments.ChangeStatus;
+using Shop.Application.Comments.Create;
+using Shop.Application.Comments.Edit;
+using Shop.Presentation.Facade.Comments;
+using Shop.Query.Comments.DTos;
+
+namespace Shop.Api.Controllers
+{
+  
+    public class CommentController : ApiController
+    {
+
+        private readonly ICommentFacad _commentFacad;
+
+        public CommentController(ICommentFacad commentFacad)
+        {
+            _commentFacad = commentFacad;
+        }
+
+        [HttpGet]
+        public async Task<ApiResult<CommentFilterResult>> GetCommentByFilter([FromQuery] CommentFilterParams filter)
+        {
+          
+            var result = await _commentFacad.GetCommentByFilter(filter);
+            return QueryResult(result);
+        }
+
+        [HttpGet("{commentId}")]
+        public async Task<ApiResult<CommentDto?>> GetCommentByFilter(long commentId)
+        {
+
+            var result = await _commentFacad.GetCommentnById(commentId);
+            return QueryResult(result);
+        }
+
+        [HttpPost]
+        public async Task<ApiResult> GetCommentByFilter(CreateCommentCommand command)
+        {
+
+            var result = await _commentFacad.CreateComment(command);
+            return CommandResult(result);
+        }
+
+        [HttpPut]
+        public async Task<ApiResult> EditComment(EditCommentCommand command)
+        {
+
+            var result = await _commentFacad.EditComment(command);
+            return CommandResult(result);
+        }
+
+        [HttpPut("ChangeStatus")]
+        public async Task<ApiResult> ChangeCommentStatus(ChangeCommentStatusCommand command)
+        {
+
+            var result = await _commentFacad.ChangeStatus(command);
+            return CommandResult(result);
+        }
+
+
+    }
+}
---
+++ Shop/Shop.Api/Controllers/OrderController.cs
@@ -0,0 +1,83 @@
+﻿using Common.Asp.NetCore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Application.Orders.AddItem;
+using Shop.Application.Orders.Checkout;
+using Shop.Application.Orders.DeacreaseItemCount;
+using Shop.Application.Orders.IncreaseItemCount;
+using Shop.Application.Orders.RemoveItem;
+using Shop.Presentation.Facade.Orders;
+using Shop.Query.Orders.DTOs;
+
+namespace Shop.Api.Controllers
+{
+  
+    public class OrderController : ApiController
+    {
+        private readonly IOrderFacad _orderFacad;
+
+        public OrderController(IOrderFacad orderFacad)
+        {
+            _orderFacad = orderFacad;
+        }
+
+        [HttpGet]
+        public async Task<ApiResult<OrderFilterResult>> GetOrderByFilter([FromQuery]OrderFilterParam filterParams)
+        {
+            var result= await _orderFacad.GetOrdersByFilter(filterParams);
+            return QueryResult(result);
+
+        }
+        [HttpGet("{orderId}")]
+        public async Task<ApiResult<OrderDto?>> GetOrderById(long orderId)
+        {
+            var result = await _orderFacad.GetOrderById(orderId);
+            return QueryResult(result);
+
+        }
+
+        [HttpPost]
+        public async Task<ApiResult> AddOrderItem(AddItemOrderCommand command)
+        {
+            
+            var result = await _orderFacad.AddOrderItem(command);
+            return CommandResult(result);
+
+        }
+
+        [HttpPost("Checkout")]
+        public async Task<ApiResult> CheckoutOrder(CheckoutOrderCommand command)
+        {
+
+            var result = await _orderFacad.OrderCheckOut(command);
+            return CommandResult(result);
+
+        }
+
+        [HttpPut("OrderItem/IncreaseCount")]
+        public async Task<ApiResult> IncreaseOrderCount(IncreaseOrderItemCountCommand command)
+        {
+
+            var result = await _orderFacad.IncreaseItemCount(command);
+            return CommandResult(result);
+
+        }
+
+        [HttpPut("OrderItem/DecreaseCount")]
+        public async Task<ApiResult> IncreaseOrderCount(DecreaseOrderItemCountCommand command)
+        {
+
+            var result = await _orderFacad.DecreaseItemCount(command);
+            return CommandResult(result);
+
+        }
+        [HttpDelete("OrderItem")]
+        public async Task<ApiResult> DeletOrderItem(RemoveOrderItemCommand command)
+        {
+
+            var result = await _orderFacad.RemoveOrderItem(command);
+            return CommandResult(result);
+
+        }
+    }
+}
